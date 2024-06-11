8da518302350ff575fed31407fa78040571eadf7
Tran Vdc <tranvanduoc2394@gmail.com>
2024-06-11 20:44:08 +0700

    created mu plugins

---
+++ wp-content/mu-plugins/load.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+ * DuocTran Core Plugin
+ *
+ * @package           DuocTranCore
+ * @author            DuocTran
+ * @copyright         DuocTran
+ * @license           GPL v2 or later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       DuocTran Core
+ * Version:           1.0.0
+ * Requires PHP:      8.1
+ * Author:            DuocTran
+ * License:           GPL v2 or later
+ */
+
+
+
+/** ========= FUNCTIONS =========== */
+
+/** ========================================== */
+/** ========= Add Must-use Plugins =========== */
+/** ========================================== */
+
+// Smart Slider 3
+require_once WPMU_PLUGIN_DIR . '/smart-slider-3/smart-slider-3.php';
---
+++ wp-content/mu-plugins/smart-slider-3.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Smart Slider 3
+Plugin URI: https://smartslider3.com/
+Description: The perfect all-in-one responsive slider solution for WordPress.
+Version: 3.5.1.21
+Requires PHP: 7.0
+Requires at least: 5.0
+Author: Nextend
+Author URI: https://smartslider3.com
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
