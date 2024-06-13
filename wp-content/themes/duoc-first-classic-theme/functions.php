ec2d95c7e39525d5ea2450992b58e842a4d744f3
Tran Vdc <tranvanduoc2394@gmail.com>
2024-06-13 22:17:30 +0700

    custom logo

--- wp-content/themes/duoc-first-classic-theme/functions.php
+++ wp-content/themes/duoc-first-classic-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 add_theme_support( 'custom-background' );
 
-
+// custom header
 function themename_custom_header_setup()
 {
     $args = array(
@@ -17,3 +17,18 @@ function themename_custom_header_setup()
 
 add_action('after_setup_theme', 'themename_custom_header_setup');
 add_theme_support( 'custom-header' );
+
+
+// custom logo
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true,
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
--- wp-content/themes/duoc-first-classic-theme/header.php
+++ wp-content/themes/duoc-first-classic-theme/header.php
@@ -1,1 +1,10 @@
+<?php
+$custom_logo_id = get_theme_mod( 'custom_logo' );
+$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+if ( has_custom_logo() ) {
+    echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+} else {
+    echo '<h1>' . get_bloginfo('name') . '</h1>';
+}
+?>
 <img alt="" src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>">
