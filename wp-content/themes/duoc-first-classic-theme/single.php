d3987987480e8df7a4079e65649107224ed0771b
Tran Vdc <tranvanduoc2394@gmail.com>
2024-06-15 16:48:00 +0700

    comments

--- wp-content/themes/duoc-first-classic-theme/single.php
+++ wp-content/themes/duoc-first-classic-theme/single.php
@@ -10,4 +10,8 @@ the_title();
     previous_post_link();
     next_post_link();
 endwhile;
+// If comments are open or we have at least one comment, load up the comment template.
+if ( comments_open() || get_comments_number() ) :
+    comments_template();
+endif;
 ?>
